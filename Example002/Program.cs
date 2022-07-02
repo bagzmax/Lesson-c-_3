b8d5a1f4f434a55c2e29f7e65f4b4ae1293a11ed
Maksim <bagzmax75@gmail.com>
2022-07-02 10:58:43 +0300

    add Example002

---
+++ Example002/Program.cs
@@ -0,0 +1,25 @@
+﻿
+void printRangeXY(float x)
+{
+    if (x == 1) 
+    {
+        System.Console.WriteLine("Плоскость 1 :X > 0 Y > 0");
+    }
+    else if (x == 2)
+    {
+        System.Console.WriteLine("плоскость 2: X < 0 Y > 0");
+    }
+    else if (x == 3)
+    {
+        System.Console.WriteLine("Плоскость 3: X < 0 Y < 0");
+    }
+    else if (x == 4)
+    {
+        System.Console.WriteLine("Плоскость 4: X > 0 Y < 0");
+    }
+}
+printRangeXY(4);
+
+
+
+
