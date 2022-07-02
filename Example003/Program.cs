effe9348c11fd375dbb07b3c9fa3a3c272947de2
Maksim <bagzmax75@gmail.com>
2022-07-02 13:54:32 +0300

    add Example004

---
+++ Example003/Program.cs
@@ -0,0 +1,9 @@
+﻿void printGipotenuza(double ax, double ay, double bx, double by)
+{
+    double deltaX = ax - bx;
+    double deltaY = ay - by;
+    double c = Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+    System.Console.WriteLine(c);
+}
+printGipotenuza(7,-5,1,-1);
+
---
+++ Example004/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine ("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while (Math.Abs(num) - i >=0)  //проверка на отрицательное вводимое число
+ {
+     Console.Write ($"{i*i},");
+     i ++;
+ }
+ Console.Write("\b \b"); // удаляет последний символ
+
+//for (int i = 1; i <= Math.Sqrt(num); i++) Console.Write($"{i*i} ");
+
