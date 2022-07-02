52454f51268611a11e848e42764c865707f9ac77
Maksim <bagzmax75@gmail.com>
2022-07-02 17:40:08 +0300

    add discription Example001

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -11,30 +11,36 @@
     else
         Console.WriteLine("Некорректные входные данные!!!");
 }
-           
-printPlanesNumber(6, 2);
+
+Console.Write("Введите координату X: ");
+int x = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите координату Y: ");
+int y = int.Parse(Console.ReadLine()!);
+
+printPlanesNumber(x, y);
 Console.WriteLine();
 
-int getPlanesNumber(float x, float y)
-{
-    if (x > 0 && y > 0)
-        return 1;
-    if (x < 0 && y > 0)
-        return 2;
-    if (x < 0 && y < 0)
-        return 3;
-    if (x > 0 && y < 0)
-        return 4;
+//int getPlanesNumber(float x, float y)
+// {
+//     if (x > 0 && y > 0)
+//         return 1;
+//     if (x < 0 && y > 0)
+//         return 2;
+//     if (x < 0 && y < 0)
+//         return 3;
+//     if (x > 0 && y < 0)
+//         return 4;
 
-    return -1;
-}
+//     return -1;
+// }
 
 
-int planesNumber = getPlanesNumber(-1, 0);
-if (planesNumber != -1 )
-    Console.WriteLine($"Плоскость {planesNumber}");
-else
-    Console.WriteLine("Некорректные входные данные!!!");
+// int planesNumber = getPlanesNumber(-1, 0);
+// if (planesNumber != -1 )
+//     Console.WriteLine($"Плоскость {planesNumber}");
+// else
+//     Console.WriteLine("Некорректные входные данные!!!");
 
 
 
